ae44d07109dc4f999a56f532d14cea087d4f6682
dragoncartoon <quocvuongtphcm@yahoo.com>
2022-11-09 16:50:23 +0700

    ready to push to Heroku

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+const dtn = require("date-fns/formatDistanceToNow");
+
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard' , messages: messages, format: dtn });
 });
+
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: "Add a new message" });
+});
+
+router.post('/new', function(req, res, next) {
+  const messageText = req.body.message;
+  const messageUser = req.body.name;
+  
+  messages.push({text: messageText, user:messageUser, added: new Date()})
+  res.redirect('/')
+})
+
 
 module.exports = router;
